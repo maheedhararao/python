7c58f9ed8f412a6fd9014ae3ed1077891e2dc584
maheedhar <44745256+maheedhararao@users.noreply.github.com>
2023-02-21 19:16:09 -0800

    Update windows_agent.py
    changed file

--- windows_agent.py
+++ windows_agent.py
@@ -2,9 +2,9 @@ import platform
 
 my_system = platform.uname()
 
-print(f"System: {my_system.system}")
-print(f"Node Name: {my_system.node}")
-print(f"Release: {my_system.release}")
-# print(f"Version: {my_system.version}")
-# print(f"Machine: {my_system.machine}")
-# print(f"Processor: {my_system.processor}")
+print("System: {my_system.system}")
+print("Node Name: {my_system.node}")
+print("Release: {my_system.release}")
+# print("Version: {my_system.version}")
+# print("Machine: {my_system.machine}")
+# print("Processor: {my_system.processor}")
