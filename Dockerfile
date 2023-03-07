abc3227227904261bae8fdcfe4044f67cbfcd2c4
maheedhar <44745256+maheedhararao@users.noreply.github.com>
2023-03-06 21:12:11 -0800

    Add files via upload
    added files

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+From ubuntu
+
+Run apt-get update
+Run apt-get install -y git
+CMD ['echo', 'Image created']
---
+++ build_image.py
@@ -0,0 +1,10 @@
+import os, time
+
+images_before = os.popen("sudo docker images").read().strip().split("\n")[1:]
+print(images_before)
+os.popen("sudo docker build -t ubuntu:v1 /home/remlab/demo")
+time.sleep(5)
+images_after = os.popen("sudo docker images").read().strip().split('\n')[1:]
+print(images_after)
+if len(images_after) <= (images_before):
+    raise Exception('Image creation is unsuccessful')
