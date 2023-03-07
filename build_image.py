d7e7b458ac264628dba280bf71ca00ca5932d534
maheedhar <44745256+maheedhararao@users.noreply.github.com>
2023-03-06 19:52:43 -0800

    Update build_image.py
    updated

--- build_image.py
+++ build_image.py
@@ -6,5 +6,5 @@ os.popen("sudo docker build -t ubuntu:v1 /home/remlab/demo")
 time.sleep(5)
 images_after = os.popen("sudo docker images").read().strip().split('\n')[1:]
 print(images_after)
-if len(images_after) <= (images_before):
+if len(images_after) >= (images_before):
     raise Exception('Image creation is unsuccessful')
